bed79f76d45dd4fa24ceab418ad26bda3ffe2bb5
unknown <nikitashatunov.aa@gmail.com>
2025-07-28 16:08:28 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('part of confil')
+    .setDescription('API for of confil')
+    .setVersion('1.3')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [AppModule],
+  });
+  SwaggerModule.setup('api', app, document);
+  app.use(bodyParser.json());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
